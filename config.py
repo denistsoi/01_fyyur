c33cdae99bb12961fe5091ab9873c95104759f6b
Denis Tsoi <denistsoi@gmail.com>
2020-04-07 17:09:34 +0800

    ADD: migrations dir;

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://denistsoi@localhost:5432/bookings'
+SQLALCHEMY_DATABASE_URI = 'postgres://denistsoi@localhost:5432/fyyur'
