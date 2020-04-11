f96abb3b9abc21a21dcaffc3e67d239c988ab8be
Denis Tsoi <denistsoi@gmail.com>
2020-04-12 02:09:25 +0800

    ADD: remove comments;

--- app.py
+++ app.py
@@ -154,9 +154,6 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
   term = request.form.get("search_term", " ")
   query = Artist.query.filter(Artist.name.ilike(f"%{term}%")).all()
   response = {
